ad09d9fed8788f76fce4430c92a5351b56bff694
Alexandr Belyaev <eminis.ru@gmail.com>
2024-06-07 15:45:48 +0300

    HOTEL-1 Implemented Binding

--- app/src/main/java/com/example/hotelapp/MainActivity.kt
+++ app/src/main/java/com/example/hotelapp/MainActivity.kt
@@ -2,46 +2,17 @@ package com.example.hotelapp
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.hotelapp.ui.theme.HotelAppTheme
+import com.example.hotelapp.databinding.ActivityMainBinding
 
 class MainActivity : ComponentActivity() {
+    private var _binding: ActivityMainBinding? = null
+    private val binding
+        get() = _binding
+            ?: throw IllegalStateException("Binding for ActivityMainBinding must not be null")
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContent {
-            HotelAppTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
-        }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    HotelAppTheme {
-        Greeting("Android")
+        _binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
