6c745404e9a0d20f6054e5d67e8f35722b086b99
Alexandr Belyaev <eminis.ru@gmail.com>
2024-06-12 13:33:53 +0300

    HOTEL-3 Display of the number of available rooms has been changed

--- app/src/main/java/com/example/hotelapp/ui/hotel/HotelFragment.kt
+++ app/src/main/java/com/example/hotelapp/ui/hotel/HotelFragment.kt
@@ -1,10 +1,14 @@
 package com.example.hotelapp.ui.hotel
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.LinearLayout
+import android.widget.TextView
+import androidx.core.view.setPadding
 import androidx.fragment.app.viewModels
 import com.bumptech.glide.Glide
 import com.example.hotelapp.R
@@ -46,7 +50,9 @@ class HotelFragment : Fragment() {
                 binding.textDistanceCount.text = item.hotel?.distance?.toString()
                 binding.ratingBarStars.rating = item.hotel?.stars ?: 0f
                 binding.hotelText.text = item.hotel?.name
-                binding.textSuitesCount.text = item.hotel?.suitesAvailability.toString()
+                item.hotel?.suitesAvailability?.let { suites ->
+                    displaySuitesAvailability(suites)
+                }
 
                 item.hotel?.image?.let { imageUrl ->
                     Glide.with(this)
@@ -63,6 +69,24 @@ class HotelFragment : Fragment() {
                         .commit()
                 }
             }
+        }
+    }
+
+    @SuppressLint("UseCompatLoadingForDrawables")
+    private fun displaySuitesAvailability(suites: String) {
+        val suitesArray = suites.split(":")
+        for (suite in suitesArray) {
+            val textView = TextView(requireContext())
+            textView.text = suite
+            textView.setPadding(16, 8, 16, 8)
+            textView.background = resources.getDrawable(R.drawable.rounded_border, null)
+            val layoutParams = LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.WRAP_CONTENT,
+                LinearLayout.LayoutParams.WRAP_CONTENT
+            )
+            layoutParams.setMargins(8, 8, 8, 8)
+            textView.layoutParams = layoutParams
+            binding.linearLayoutSuites.addView(textView)
         }
     }
 }
