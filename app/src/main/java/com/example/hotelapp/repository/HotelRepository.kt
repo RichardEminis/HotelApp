90b650ea0f73ced8152a084b6d57defdaeca1d6a
Eminis <eminis.ru@gmail.com>
2024-06-12 22:46:15 +0300

    HOTEL-3 Edited HotelRepository

--- app/src/main/java/com/example/hotelapp/repository/HotelRepository.kt
+++ app/src/main/java/com/example/hotelapp/repository/HotelRepository.kt
@@ -1,19 +1,11 @@
 package com.example.hotelapp.repository
 
-import android.util.Log
 import com.example.hotelapp.model.Hotel
 import com.example.hotelapp.model.HotelDetail
 import com.example.hotelapp.network.ApiService
 import com.example.hotelapp.utils.HOTEL_DETAIL_URL
-import com.example.hotelapp.utils.HOTEL_LIST_URL
-import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
-import kotlinx.serialization.json.Json
-import okhttp3.MediaType.Companion.toMediaType
-import okhttp3.OkHttpClient
-import okhttp3.logging.HttpLoggingInterceptor
-import retrofit2.Retrofit
 import javax.inject.Inject
 import kotlin.coroutines.CoroutineContext
 
